29c12e248703e89b69d5236e09a7ace7bffd064f
Heitor Pupim <heitorpupim@MacBook-Pro-de-Heitor.local>
2025-12-12 16:19:51 -0300

    chore: add eslint sort pugin and prettier tailwind plugin

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 const ProductsPage = () => {
   return (
-    <div className="p-5 border border-red-500 rounded-xl">
+    <div className="border border-red-500 rounded-xl p-5">
       <h1 className="text-red-500">Products Page</h1>
       <Button>FS</Button>
       <Input placeholder="Search"/>
