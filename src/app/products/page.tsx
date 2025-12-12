862b056ae8b8e68a45cd2fa156683816e4c01e44
Heitor Pupim <heitorpupim@MacBook-Pro-de-Heitor.local>
2025-12-12 16:09:09 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductsPage = () => {
+  return (
+    <div className="p-5 border border-red-500 rounded-xl">
+      <h1 className="text-red-500">Products Page</h1>
+      <Button>FS</Button>
+      <Input placeholder="Search"/>
+
+
+    </div>
+  );
+}
+ 
+export default ProductsPage;
