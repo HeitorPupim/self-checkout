224aceae9673a6fcb682ee8fbc730a5a65f6a3fb
Heitor Pupim <heitorpupim@MacBook-Pro-de-Heitor.local>
2025-12-15 15:07:29 -0300

    chore: ready to deploy

--- src/app/[slug]/menu/components/products.tsx
+++ src/app/[slug]/menu/components/products.tsx
@@ -8,40 +8,42 @@ interface ProductsProps {
   products: Product[];
 }
 
-
-const Products = ({products}: ProductsProps) => {
-  const {slug} = useParams<{slug: string}>();
+const Products = ({ products }: ProductsProps) => {
+  const { slug } = useParams<{ slug: string }>();
   const searchParams = useSearchParams();
   const consumptionMethod = searchParams.get("consumptionMethod");
 
-
-  return (  
-    <div className="space-y-3 px-5 py-3">
-    {products.map((product) => (
-      <Link key = {product.id} 
-      href={`/${slug}/menu/${product.id}?consumptionMethod=${consumptionMethod}`} 
-      className="flex items-center justify-between gap-10 py-3 border-blue-50">
-        <div>
-        {/* Esquerda */}
-          <h3 className="text-sm font-medium">{product.name}</h3>
-          <p className="line-clamp-2 text-sm text-muted-foreground">{product.description}</p>
-          <p className="pt-3 text-sm font-semibold">{formatCurrency(product.price)} </p>
-        </div>
-        {/* Direita */}
-        <div className="relative min-h-[82px] min-w-[126px]">
-          <Image
-            src={product.imageUrl}
-            alt={product.name}
-            fill
-            className="rounded-lg object-contain"
-          />
-        </div>
-
-        
-      </Link>
-    ))} 
+  return (
+    <div className="space-y-3 px-5 py-3 ">
+      {products.map((product) => (
+        <Link
+          key={product.id}
+          href={`/${slug}/menu/${product.id}?consumptionMethod=${consumptionMethod}`}
+          className="flex items-center justify-between gap-10 border-blue-50 py-3"
+        >
+          <div>
+            {/* Esquerda */}
+            <h3 className="text-sm font-medium">{product.name}</h3>
+            <p className="line-clamp-2 text-sm text-muted-foreground">
+              {product.description}
+            </p>
+            <p className="pt-3 text-sm font-semibold">
+              {formatCurrency(product.price)}{" "}
+            </p>
+          </div>
+          {/* Direita */}
+          <div className="relative min-h-[82px] min-w-[126px]">
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              fill
+              className="rounded-lg object-contain"
+            />
+          </div>
+        </Link>
+      ))}
     </div>
   );
-}
- 
+};
+
 export default Products;
